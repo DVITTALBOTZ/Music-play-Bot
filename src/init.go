828567cbc2652c87c897ef45e3f886fccf3ba6a4
DVITTALBOTZ <133628140+DVITTALBOTZ@users.noreply.github.com>
2026-03-01 18:30:35 +0530

    Add files via upload

---
+++ src/init.go
@@ -0,0 +1,37 @@
+/*
+ * TgMusicBot - Telegram Music Bot
+ *  Copyright (c) 2025-2026 TEAMDEV
+ *
+ *  Licensed under GNU GPL v3
+ *  See https://github.com/justfortestingnothibghere/TgMusicBot
+ */
+
+package src
+
+import (
+	"ashokshau/tgmusic/config"
+	"ashokshau/tgmusic/src/core/db"
+	"ashokshau/tgmusic/src/handlers"
+	"ashokshau/tgmusic/src/vc"
+	"context"
+
+	tg "github.com/amarnathcjd/gogram/telegram"
+)
+
+func Init(client *tg.Client) error {
+	if err := db.InitDatabase(context.Background()); err != nil {
+		return err
+	}
+
+	for _, session := range config.Conf.SessionStrings {
+		_, err := vc.Calls.StartClient(config.Conf.ApiId, config.Conf.ApiHash, session)
+		if err != nil {
+			return err
+		}
+	}
+
+	vc.Calls.RegisterHandlers(client)
+	handlers.LoadModules(client)
+
+	return nil
+}
